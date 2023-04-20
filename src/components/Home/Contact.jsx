b72c3cb1be0628da7b2d68b1dc62eb9a0057538c
alpachm <alexpacheco407@gmail.com>
2023-04-19 22:25:34 -0500

    form

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -7,17 +7,20 @@ const Contact = () => {
       <div className="container-contact grilla">
         <h2 className="subtitulo">Envíanos un email</h2>
 
-        <form>
-          <input type="text" placeholder="Nombre" />
-          <input type="text" placeholder="Número de contácto" />
-          <input type="text" placeholder="Motivo" />
+        <form action="https://formsubmit.co/solotechosbogota@gmail.com" method="POST">
+          <input name="Nombre" type="text" placeholder="Nombre" required />
+          <input name="Número" type="text" placeholder="Número de contácto" required />
+          <input name="Motivo" type="text" placeholder="Motivo" required />
           <textarea
-            name=""
+            name="Texto"
             placeholder="Dejános un mensaje"
             cols="30"
             rows="7"
           ></textarea>
           <button>Envíar</button>
+
+          <input type="hidden" name="_next" value="https://solotechosbogota.com/"></input>
+          <input type="hidden" name="_captcha" value="false"></input>
         </form>
       </div>
     </section>
