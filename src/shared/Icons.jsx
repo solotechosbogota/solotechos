d42be0a8405edc70bcb66332eb6c733a620f155b
alpachm <alexpacheco407@gmail.com>
2023-05-04 18:43:34 -0500

    phone icon flotante

--- src/shared/Icons.jsx
+++ src/shared/Icons.jsx
@@ -4,8 +4,8 @@ import './styles/icons.css';
 const Icons = () => {
   return (
     <div className="icons">
-      <a href="https://www.facebook.com/solotechosbogota" target="_blank">
-        <i className="bx bxl-facebook-circle"></i>
+      <a href="tel: 601 6278562">
+        <i className="bx bxs-phone"></i>
       </a>
       <hr />
       <a href="https://www.instagram.com/solotechosbogota/" target="_blank">
