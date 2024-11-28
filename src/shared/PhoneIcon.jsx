a85ec8c9c8eb784eff078c519df553eacc930da2
alpachm <alexpacheco407@gmail.com>
2024-11-27 22:37:41 -0400

    agregar nueva imagen de contacto

--- src/shared/PhoneIcon.jsx
+++ src/shared/PhoneIcon.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles/phoneIcon.css';
-import imgPhoneIcon from '../assets/icon-phone.png';
+import imgPhoneIcon from '../assets/icon-phone-2.png';
 
 const PhoneIcon = () => {
   return (
-    <a className="phone-icon" href="tel: 3138127333">
+    <a className="phone-icon" href="tel: 3203496061">
       <img src={imgPhoneIcon} alt="" />
     </a>
   );
